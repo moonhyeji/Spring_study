4eea5c6a82a3896cd79a91e86b6214b3cc47a120
Moon0127 <mhj5601@gmail.com>
2021-05-25 23:38:45 +0900

    springmvc
    spring을 이용한 mvc 패턴 공부

---
+++ SpringMVC02/src/main/java/com/mvc/mapping/HomeController.java
@@ -0,0 +1,84 @@
+package com.mvc.mapping;
+
+import java.text.DateFormat;
+import java.util.Date;
+import java.util.Locale;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.mvc.mapping.dto.AddressDto;
+
+
+/**
+ * Handles requests for the application home page.
+ */
+@Controller
+public class HomeController {
+	
+	private static final Logger logger = LoggerFactory.getLogger(HomeController.class);
+	
+	/**
+	 * Simply selects the home view to render by returning its name.
+	 */
+	@RequestMapping(value = "/home.do", method = RequestMethod.GET)
+	public String home(Locale locale, Model model) {
+		logger.info("Welcome home! The client locale is {}.", locale);
+		
+		Date date = new Date();
+		DateFormat dateFormat = DateFormat.getDateTimeInstance(DateFormat.LONG, DateFormat.LONG, locale);
+		
+		String formattedDate = dateFormat.format(date);
+		
+		model.addAttribute("serverTime", formattedDate );
+		
+		return "home";
+	}
+	
+	/*
+	@RequestMapping(value="/command.do", method=RequestMethod.GET)
+	public String getCommand(Model model, String name, @RequestParam("addr") String address, String phone) {
+		
+		model.addAttribute("dto", new AddressDto(name, address, phone));
+		
+		return "getCommand";  // 얘가 view name
+	}
+	*/
+	
+	@RequestMapping(value="/command.do", method=RequestMethod.GET)
+	public String getCommand(Model model, AddressDto dto) {
+		
+		model.addAttribute("dto", dto);
+		
+		return "getCommand";  //얘가 view name
+	}
+	
+	
+	
+	@RequestMapping(value="/command.do", method=RequestMethod.POST)
+    public String postCommand(Model model, @ModelAttribute AddressDto dto) {
+    	  
+		model.addAttribute("dto", dto);
+    	   
+    	   return "postcommand";  //postcommand.jsp  가 됨. 
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SpringMVC02/src/main/webapp/WEB-INF/views/postcommand.jsp
@@ -0,0 +1,28 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+
+
+	<h1>POST</h1>
+	
+	<table border="1">
+	   <tr>
+	      <th>NAME</th>
+	      <th>ADDRESS</th>
+	      <th>PHONE</th>
+	   </tr>
+	   <tr>
+	      <th>${dto.name }</th>
+	      <th>${dto.addr }</th>
+	      <th>${dto.phone }</th>
+	   </tr>
+	</table>
+
+</body>
+</html>
