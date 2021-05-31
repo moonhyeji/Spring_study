4b2f7c90c6a950e75b45411174081b6f44472b81
Moon0127 <mhj5601@gmail.com>
2021-05-31 22:36:50 +0900

    SpringBoot study03
    <html xmlns:th="http://www.thymeleaf.org">
    1.hello
    2.utility
    3.expression
    4.parameters
    5.static

---
+++ StartBoot03/src/main/java/com/boot/leaf/dto/LeafDto.java
@@ -0,0 +1,36 @@
+package com.boot.leaf.dto;
+
+public class LeafDto {
+
+	private String subject;
+	private int seq;
+	public LeafDto() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	public LeafDto(String subject, int seq) {
+		super();
+		this.subject = subject;
+		this.seq = seq;
+	}
+	
+	
+	public String getSubject() {
+		return subject;
+	}
+	public void setSubject(String subject) {
+		this.subject = subject;
+	}
+	public int getSeq() {
+		return seq;
+	}
+	public void setSeq(int seq) {
+		this.seq = seq;
+	}
+	@Override
+	public String toString() {
+		return "LeafDto [subject=" + subject + ", seq=" + seq + "]";
+	}
+	
+	
+}
